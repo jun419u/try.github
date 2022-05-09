fbca85fa35a77f6133e6b7005779ae095d6a5493
Toshi7 <ttoshi0719@gmail.com>
2022-05-09 17:24:36 +0900

    掛け算と割り算追加

---
+++ try.github/try.github/Division.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_github
+{
+    public class Division
+{
+        public int division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
