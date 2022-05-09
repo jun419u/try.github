9714797a0c8b9fffaa6c3ad26116b2144dcc1f0c
kkz1022 <kakazu-souta-1022@occ.co.jp>
2022-05-09 17:35:34 +0900

    Mainくらす編集しました

--- try.github/try.github/Program.cs
+++ try.github/try.github/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Num1 = 15;
+int Num2 = 5;
+Console.WriteLine("値を2つセットします");
+Console.WriteLine("1つ目は" + Num1);
+Console.WriteLine("2つ目は" + Num2);
+Addition sum = new Addition(Num1, Num2);
+Subtraction mainasu = new Subtraction(Num1, Num2);
+Mutiplication kakeru = new Mutiplication(Num1, Num2);
+Division waru = new Division(Num1, Num2);
+Console.WriteLine(Num1 + " + " + Num2 + "=" + sum());
+
+Console.WriteLine(Num1 + " - " + Num2 + " = "mainasu());
+
+Console.WriteLine(Num1 + " × " + Num2 + " = "kakeru());
+
+Console.WriteLine(Num1 + " ÷ " + Num2 + " = "waru());
