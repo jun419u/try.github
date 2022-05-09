b9f9843914fa24684a9a37e481b9a1515984d173
jun419u <ishikawa-jyunnosuke-2212@occ.co.jp>
2022-05-09 16:53:50 +0900

    足し算クラス

--- try.github/try.github/Program.cs
+++ try.github/try.github/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Add
+{
+
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+}
